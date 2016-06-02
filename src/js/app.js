823992fbe17feccccda6529d64b7b3e22f39c313
progrape <chility@qq.com>
2016-06-02 19:45:03 +0800

    fastclick

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 import Router from 'router';
+import attachFastClick from 'fastclick';
 import 'weui';
 import './app.less';
 import Home from './home/home';
 import Register from './register/register';
 
-const router = new Router();
+attachFastClick.attach(document.body);
 
+const router = new Router();
 router.push(Home).push(Register).setDefault('/').init();
