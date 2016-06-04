b1c49f7c750b8a5e9174dab8ddabfc1c822cac35
progrape <chility@qq.com>
2016-06-04 17:36:22 +0800

    setting

---
+++ src/js/setting/setting.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+import 'weui.js';
+import tpl from 'raw!./setting.html';
+import styles from './setting.less';
+
+export default {
+    url: '/setting',
+    className: 'setting',
+    render: function () {
+        return tpl;
+    },
+    bind: function () {
+
+    }
+};
