a233c44665cf528faf440fc78a30e2a0842adf17
progrape <chility@qq.com>
2016-06-04 16:33:24 +0800

    get vconsole async

--- src/js/app.js
+++ src/js/app.js
@@ -10,3 +10,5 @@ attachFastClick.attach(document.body);
 
 const router = new Router();
 router.push(Home).push(Register).push(Profile).setDefault('/').init();
+
+$.getScript('https://rawgit.com/wechatfe/vconsole/master/dist/vconsole.min.js');
