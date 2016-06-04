a3c752415934246570d1773d3fab7ed739e671a7
progrape <chility@qq.com>
2016-06-04 16:27:50 +0800

    profile

--- src/js/profile/profile.js
+++ src/js/profile/profile.js
@@ -1,15 +1,34 @@
 import $ from 'jquery';
 import 'weui.js';
+import template from 'art-template/dist/template-debug';
 import tpl from 'raw!./profile.html';
-import style from './profile.less';
+import styles from './profile.less';
 
 export default {
     url: '/profile',
     className: 'profile',
     render: function () {
-        return tpl;
+        return template.compile(tpl)({
+            styles: styles
+        });
     },
     bind: function () {
+        $(this).on('click', '#avatarCell', function () {
 
+            $.weui.actionSheet([
+                {
+                    label: '拍照',
+                    onClick: () => {}
+                },
+                {
+                    label: '从手机相册选择',
+                    onClick: () => {}
+                },
+                {
+                    label: '保存图片',
+                    onClick: () => {}
+                }
+            ]);
+        });
     }
 };
