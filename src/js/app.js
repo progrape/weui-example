dc0c5d6a59bfa71ab4ae186088ae01df0a2206ce
progrape <chility@qq.com>
2016-06-05 15:35:32 +0800

    share

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,33 @@ attachFastClick.attach(document.body);
 
 const router = new Router();
 router.push(Home).push(Register).push(Profile).push(Setting).setDefault('/').init();
+
+$.getJSON('https://weui.io/api/sign?url=' + location.href).success((res) => {
+    wx.config({
+        debug: true,
+        appId: res.appid,
+        timestamp: res.timestamp,
+        nonceStr: res.nonceStr,
+        signature: res.signature,
+        jsApiList: [
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage',
+            'onMenuShareQQ',
+            'onMenuShareWeibo',
+            'onMenuShareQZone'
+        ]
+    });
+
+    wx.onMenuShareAppMessage({
+        title: 'WeUI',
+        desc: 'WeUI, 为微信 Web 服务量身设计', // 分享描述
+        link: 'https://weui.io/example', // 分享链接
+        imgUrl: 'https://mmrb.github.io/avatar/bear.jpg', // 分享图标
+        success: function () {
+
+        },
+        cancel: function () {
+
+        }
+    });
+});
