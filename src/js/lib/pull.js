2d472cac760d6113e2be09edbe94c0d42a4c035a
progrape <chility@qq.com>
2016-06-08 12:47:42 +0800

    pull to refresh

--- src/js/lib/pull.js
+++ src/js/lib/pull.js
@@ -3,22 +3,41 @@ import $ from 'jquery';
 $.fn.pull = function (options) {
 
     const defaults = $.extend({
-        onRefresh: (callback) => {callback(null);}
+        onRefresh: (callback) => {
+            callback(null);
+        }
     }, options);
 
     let start;
+    let diff;
     $(this).on('touchstart', function (e) {
         start = e.originalEvent.touches[0].pageY;
         $('.weui_pull_tips').find('span').text('下拉刷新').siblings('i').hide();
     }).on('touchmove', function (e) {
         const move = e.originalEvent.touches[0].pageY;
-        const diff = move - start - 40;
-        $(this).css('transition', 'none').css('transform', 'translateY(' + diff + 'px)');
+        diff = move - start - 40;
+        console.log($(this).scrollTop());
+        if (diff + 40 > 0) {
+            $(this).css('-webkit-transition', 'none')
+                .css('transition', 'none')
+                .css('-webkit-transform', 'translateY(' + diff + 'px)')
+                .css('transform', 'translateY(' + diff + 'px)');
+        }
     }).on('touchend', function (e) {
-        $(this).css('transition', 'all ease .3s').css('transform', 'translateY(0)');
-        $('.weui_pull_tips').find('span').text('刷新中').siblings('i').show();
-        defaults.onRefresh((err) => {
-            $(this).css('transition', 'all ease .3s').css('transform', 'translateY(-40px)');
-        });
+        if (diff + 40 > 0) {
+            $(this)
+                .css('-webkit-transition', 'all ease .3s')
+                .css('transition', 'all ease .3s')
+                .css('-webkit-transform', 'translateY(0)')
+                .css('transform', 'translateY(0)');
+            $('.weui_pull_tips').find('span').text('刷新中').siblings('i').show();
+            defaults.onRefresh((err) => {
+                $(this)
+                    .css('-webkit-transition', 'all ease .3s')
+                    .css('transition', 'all ease .3s')
+                    .css('-webkit-transform', 'translateY(-40px)')
+                    .css('transform', 'translateY(-40px)');
+            });
+        }
     });
 };
